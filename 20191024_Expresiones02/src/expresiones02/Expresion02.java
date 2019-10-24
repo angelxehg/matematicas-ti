2169b67d5970c955e63ba4042e70c02f9e62b1ee
Angel Hurtado <angelxehg@outlook.com>
2019-10-24 18:31:04 -0500

    Rojos

--- 20191024_Expresiones02/src/expresiones02/Expresion02.java
+++ 20191024_Expresiones02/src/expresiones02/Expresion02.java
@@ -23,11 +23,14 @@ public class Expresion02 {
         double m = 6;
         double n = (double) 1 / 4;
         // Realizar operaciones
-        double r1 = 0;
-        double r2 = 0;
-        double r3 = 0;
-        double r4 = 0;
-        double r5 = 0;
+        double r1 = (c * Math.sqrt(3 * a)) - (d * Math.sqrt(16 * b * b)) + (n * Math.sqrt(8 * d));
+        double r2 = (((double) 1 / 2) * ((double) 4 * Math.pow(d, 2)))
+                + (((double) 1 / 2) * ((double) 16 * Math.pow(n, 2))) - 1;
+        double r3 = ((double) (a + b) / (c)) - ((double) (b + m) / (d));
+        double r4 = ((double) (Math.sqrt(b) + Math.sqrt(2 * b)) / 2)
+                - ((double) (Math.sqrt(3 * c) + Math.sqrt(8 * d)) / 4);
+        double r5 = ((double) (2 * Math.sqrt(Math.pow(a, 2) * Math.pow(b, 2))) / 3)
+                + ((double) (3 * Math.sqrt(2 + Math.pow(d, 2))) / 4) - (a * Math.sqrt(n));
         // Imprimir valores
         System.out.println("Resultado 1: " + r1);
         System.out.println("Resultado 2: " + r2);
