22805493a09dbd6b0afe1ecab0415f3899a79584
Angel Hurtado <angelxehg@outlook.com>
2019-10-31 17:43:47 -0600

    Añadir clases

---
+++ 20191031_ManipulacionCadenas/src/manipulaciones/Entrada.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manipulaciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author angel
+ */
+public class Entrada {
+
+    private Scanner s;
+
+    public Entrada() {
+        s = new Scanner(System.in);
+    }
+
+    public void mostrarMensaje(String m) {
+        System.out.println(m);
+    }
+
+    public int numeroEntero() {
+        try {
+            return this.s.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("InputMismatchException: " + e.getMessage());
+            return -1;
+        }
+    }
+}
