95536982e897360e3ae53af814784ee12334280e
Angel Hurtado <angelxehg@outlook.com>
2019-11-06 00:37:19 -0600

    Separar en arraylist

--- 20191105_Expresivo/src/expresivo/Main.java
+++ 20191105_Expresivo/src/expresivo/Main.java
@@ -5,6 +5,8 @@
  */
 package expresivo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author utzac
@@ -16,6 +18,39 @@ public class Main {
      */
     public static void main(String[] args) {
         Entrada entrada = new Entrada();
+        String expresion = entrada.cadena().trim();
+        // ...
+        ArrayList<String> fragmentos = Main.separar(expresion);
+        //
+        System.out.println("Expresión: " + expresion);
+        for (String fragmentp : fragmentos) {
+            System.out.println(fragmentp);
+        }
     }
-    
+
+    public static ArrayList<String> separar(String expresion) {
+        int nextPlus = expresion.indexOf("+");
+        int nextMinus = expresion.indexOf("-");
+        if (nextPlus < 0 && nextMinus < 0) {
+            // Caso base
+            ArrayList<String> fragmentos = new ArrayList<>();
+            fragmentos.add(expresion);
+            return fragmentos;
+        }
+        int index = 0;
+        if ((nextPlus < nextMinus && nextPlus > 0) || nextMinus < 0) {
+            // Suma está más cerca
+            index = nextPlus;
+        } else {
+            // Resta está más cerca
+            index = nextMinus;
+        }
+        // Recursividad
+        String fragmento = expresion.substring(0, index);
+        String restante = expresion.substring(index + 1);
+        ArrayList<String> fragmentos = Main.separar(restante);
+        fragmentos.add(fragmento);
+        return fragmentos;
+    }
+
 }
